f229906d31453b56863004f868b764a9e9435e33
evan chan <evanchan52.designs@gmail.com>
2021-03-11 23:50:41 -0500

    database n images

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // This is where we would add our middleware
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-router.use('/api', createProxyMiddleware({
+router.use('/roku', createProxyMiddleware({
     target: 'http://localhost:5000',
     headers: {
         accept: 'application/json, application/x-www-form-urlencoded'
@@ -13,13 +13,27 @@ router.use('/api', createProxyMiddleware({
 }))
 
 router.get('/', (req, res) => {
-    res.render('index', { message: "Hi there! From handlebars!" })
+    res.render('index',
+        {
+            message: "Hi there! From handlebars!"
+        })
+})
+
+router.get('/users', (req, res) => {
+    res.render('index',
+        {
+            message: "Hi there, users!"
+        })
 })
 
 // Other routes that you might use
 router.use((req, res) => {
     res.status(404);
-    res.render("error", { layout: "errorLayout.hbs", errormessage: `You are in the Error Zone! "${req.url}" doesn't exist.` });
+    res.render("error",
+        {
+            layout: "errorLayout.hbs",
+            errormessage: `You are in the Error Zone! "${req.url}" doesn't exist.`
+        });
 })
 
 module.exports = router;
